51ed29debd28b91df7a12bab623628b7840353d8
Gustavo Nobrega <gustavo.nobrega@hotmail.com>
2020-09-17 10:53:31 -0300

    add architecture principles and tests

---
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
+
+import UsersController from '../controllers/UsersController';
+import UserAvatarController from '../controllers/UserAvatarController';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+const usersRouter = Router();
+const usersControllers = new UsersController();
+const userAvatarController = new UserAvatarController();
+const upload = multer(uploadConfig);
+
+usersRouter.post('/', usersControllers.create);
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  userAvatarController.update,
+);
+
+export default usersRouter;
